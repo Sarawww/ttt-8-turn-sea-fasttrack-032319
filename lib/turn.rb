227f648359aa3e4a556922ec4233f6eb8f64815b
concurrent-input-6175 <xywong23@gmail.com>
2019-03-24 19:53:07 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -28,7 +28,7 @@ def move(board, index, current_player = "X")
 end
 
 def turn(board)
-  puts "please enter 1-9:"
+  puts "Please enter 1-9:"
   user_input = gets.strip
   index = input_to_index(user_input)
    if valid_move?(board, index)
